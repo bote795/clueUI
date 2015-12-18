9d02f46b0c17b0f5ebb090b948ce86a6df8ea183
bote795 <nbotell2@gmail.com>
2015-12-18 17:24:42 -0600

    able to submit data
    need to fix submission and redrawing.. since it doesn't lead duplicates
    happen. When re drawing aka looping

--- Javascript/clue.js
+++ Javascript/clue.js
@@ -9,7 +9,7 @@ angular.module('clueApp', [])
     //list of possible wheres
     clueList.where =["Bathroom","Office","Dining Room","Game Room","Garage","Bedroom","Living Room","Kitchen","Courtyard"];
     //list of options for player
-    clueList.options= ["skip", "stoped"];
+    clueList.options= ["skip", "stoped", "None"];
     //number of players
     clueList.num=3;
     //player names
@@ -28,22 +28,23 @@ angular.module('clueApp', [])
       clueList.users.push(clueList.username);
       console.log(clueList.username);
       clueList.username = "";
-      clueList.keys.push(clueList.id(clueList.users.length+1))
+      clueList.keys.push(clueList.id(clueList.users.length))
       if(clueList.users.length == clueList.num)
         {clueList.inputComplete = true;}
     };
     
 
  
-    clueList.addClue = function() {
+    clueList.addClue = function(form) {
+      console.log(form);
       var temp =[clueList.turn,
-      clueList.player, clueList.who_clue ,clueList.what_clue,
-      clueList.where_clue];
+      form.player, form.who_clue ,form.what_clue,
+      form.where_clue];
       clueList.detectiveClues.push(temp);
-
-      for (var i = 0; i < clueList.users.length; i++) {
-        var id="player"+clueList.users[i];
-        temp.push(clueList[id]);
+      console.log(clueList.detectiveClues);
+      for (var i = 1; i <= clueList.users.length; i++) {
+        var id="player"+i;
+        temp.push(form[id]);
       };
       //save row
       clueList.turn++;
